6578b1534f243a961433db3e6528eeb338c95d75
TALAT <medasmac1@TalatMacPro.local>
2018-08-10 18:10:19 +0400

    resourse slot check

--- controllers/appointment.js
+++ controllers/appointment.js
@@ -63,6 +63,7 @@ function getResourceSlots(post_data, next){
     const params = [post_data.appoint_date, post_data.appoint_date, post_data.resource_ids, date.getDay()];
 
     const join_query = apt_query.queryResourceSlots();
+    console.log(join_query, params);
     db_query.paramQuery(join_query, params, (err, result)=>{
         if(err) return next(err);  
         return next(null,result);
@@ -78,6 +79,8 @@ function getDistinctResourceSlots(post_data, next) {
             getResourceSlots(post_data, (err, resouceSlots)=>{
                 if(err) return next(err);
                 const minMax = helper.getMinMaxTime(resouceSlots);
+                console.log(resouceSlots);
+                if(!resouceSlots.length) return next("no resourceSlot found")
                 // console.log(helper.getMinMaxTime(resouceSlots));
                 // console.log(helper.makeSlots(minMax.min,minMax.max, resouceSlots[0].intrvl));
                 next(null, helper.makeSlots(minMax.min,minMax.max, resouceSlots[0].intrvl));
